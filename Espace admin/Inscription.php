10362f7aefb0c3540eabcfdaf52fd75f274dbf3a
Mortann <113791876+Mortann@users.noreply.github.com>
2022-09-24 21:23:52 +0200

    maj

--- Espace admin/Inscription.php
+++ Espace admin/Inscription.php
@@ -6,13 +6,7 @@
             $insertUser = $bdd->prepare('INSERT INTO users(pseudo, mdp)VALUES(?, ?)');
             $insertUser->execute(array($pseudo, $mdp));
     
-            $recupUser = $bdd->prepare('SELECT * FROM users WHERE pseudo = ? AND mdp = ?');
-            $recupUser->execute(array($pseudo, $mdp));
-            if($recupUser->rowCount() > 0){
-                $_SESSION['pseudo'] = $pseudo;
-                $_SESSION['mdp'] = $mdp;
-                $_SESSION['id'] = $recupUser->fetch()['id'];
-            }
+
     
         }else{
             echo "Veuillez compléter tous les champs ...";
