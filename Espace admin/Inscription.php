5674694367297a9016c845f7f6d4baff361f7355
Mortann <113791876+Mortann@users.noreply.github.com>
2022-09-24 21:35:06 +0200

    maj

--- Espace admin/Inscription.php
+++ Espace admin/Inscription.php
@@ -36,16 +36,16 @@ if(!$_SESSION['mdp']){
         <br>
         <h2>IDENTITÉ</h2>
         <br><h3>Clan :</h3>
-        <input type="checkbox"name="CDT" id="demo2">
+        <input type="checkbox"name="demo2" id="Clan">
         <label for="demo2">Clan du Tonnère</label>  <br><br>
         
-        <input type="checkbox" name="CDV" id="demo2">
+        <input type="checkbox" name="demo2" id="Clan">
         <label for="demo2"> Clan du Vent</label>  <br><br>
         
-        <input type="checkbox" name="CDR" id="demo2">
+        <input type="checkbox" name="demo2" id="Clan">
         <label for="demo2">Clan de la Rivière</label>  <br><br>
         
-        <input type="checkbox" name="CDO" id="demo2">
+        <input type="checkbox" name="demo2" id="Clan">
         <label for="demo2">Clan de l'Ombre</label>  
 
         <br><h3>Nom Chaton :</h3>
